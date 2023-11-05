e29abdfea1d1e19dcff883b685c05826a2a622bc
evanightly <adamhenderson3x3@gmail.com>
2023-11-05 17:09:48 +0700

    Refactor darkMode

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:client/config/custom_theme.dart';
+import 'package:client/providers/settings_provider.dart';
+import 'package:client/providers/shared_preference_provider.dart';
 import 'package:client/screens/admin/admin_dashboard_screen.dart';
 import 'package:client/screens/admin/admin_login_screen.dart';
 import 'package:client/screens/home_screen.dart';
@@ -8,34 +10,33 @@ import 'package:client/screens/user_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences_riverpod/shared_preferences_riverpod.dart';
-
-late SharedPreferences prefs;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
 
-  prefs = await SharedPreferences.getInstance();
-
-  runApp(const ProviderScope(child: App()));
+  runApp(ProviderScope(child: App(prefs)));
 }
 
-final themeProvider = createPrefProvider<bool>(
-  prefs: (_) => prefs,
-  prefKey: "darkMode",
-  defaultValue: false,
-);
+// final themeProvider = createPrefProvider<bool>(
+//   prefs: (_) => prefs,
+//   prefKey: "darkMode",
+//   defaultValue: false,
+// );
 
 class App extends ConsumerWidget {
-  const App({super.key});
+  final SharedPreferences prefs;
+  const App(this.prefs, {super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.read(sharedPreferenceProvider.notifier).init(prefs);
     return MaterialApp(
       title: 'Named Routes Demo',
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
-      theme: ref.watch(themeProvider) ? darkThemeData : lightThemeData,
+      theme:
+          ref.watch(settingsProvider).darkMode ? darkThemeData : lightThemeData,
       routes: {
         '/': (context) => const HomeScreen(),
         '/admin/login': (context) => const AdminLoginScreen(),
--- client/lib/screens/settings_screen.dart
+++ client/lib/screens/settings_screen.dart
@@ -1,4 +1,4 @@
-import 'package:client/main.dart';
+import 'package:client/providers/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,9 +11,9 @@ class SettingsScreen extends ConsumerWidget {
       children: [
         SwitchListTile.adaptive(
           title: const Text('Dark Mode'),
-          value: ref.watch(themeProvider),
+          value: ref.watch(settingsProvider).darkMode,
           onChanged: (changeTo) async {
-            await ref.read(themeProvider.notifier).update(changeTo);
+            ref.read(settingsProvider.notifier).toggleDarkMode(changeTo);
           },
         ),
       ],
